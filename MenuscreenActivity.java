88211a57639498fbbd20369eaf3c72a0857c1456
Kristian1004 <98990666+Kristian1004@users.noreply.github.com>
2022-03-17 12:38:29 -0400

    Add files via upload

---
+++ MenuscreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.minilibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menuscreen);
+    }
+}
