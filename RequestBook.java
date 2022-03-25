a7ac6152919d2c88149b78e96d4b0753d7c9cb94
Methunancodes-20 <77935285+Methunancodes-20@users.noreply.github.com>
2022-03-25 18:51:46 -0400

    Add files via upload

---
+++ RequestBook.java
@@ -0,0 +1,14 @@
+package com.example.resultbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequestBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.RequestBook);
+    }
+}
