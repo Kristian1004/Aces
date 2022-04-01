136ef347f8b3552681dd673bb46ddf18124ba012
Kristian1004 <98990666+Kristian1004@users.noreply.github.com>
2022-04-01 15:40:38 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -2,7 +2,9 @@ package com.example.minilibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void goNextPage(View view) {
+        startActivity(new Intent(MainActivity.this, MenuscreenActivity.class));
+    }
+    public void goBB(View view) {
+        startActivity(new Intent(MainActivity.this, browsebooks.class));
+    }
+
 }
