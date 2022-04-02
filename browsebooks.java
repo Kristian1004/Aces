126c1be77738dcd27e413f296dbca6cf4700b434
shiva2734 <99999819+shiva2734@users.noreply.github.com>
2022-04-02 19:58:17 -0400

    Update browsebooks.java

--- browsebooks.java
+++ browsebooks.java
@@ -1,14 +1,41 @@
-package com.example.myapplication;
+package com.example.books;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;;
 
-public class browsebooks extends AppCompatActivity {
+public class browsebooksActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.browsebooks);
+        setContentView(R.layout.browsebooksActivity);
+        goBrowseBooks();
+        goRequestBooks();
+
+    }
+
+    private void goRequestBooks(){
+        Button button = (Button) findViewById(R.id.RequestBook);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(browsebooksActivity.this, RequestActivity.class));
+            }
+        });
+    }
+
+    private void goBrowseBooks(){
+        ImageButton button = (ImageButton) findViewById(R.id.imageButton9);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(browsebooksActivity.this, WimpyKidActivity.class));
+            }
+        });
     }
 }
