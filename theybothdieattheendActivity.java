d2ec3749501ed8d7532d7a0f5b041803a697a00a
ahlaamali <97645050+ahlaamali@users.noreply.github.com>
2022-04-02 14:16:47 -0400

    Add files via upload

---
+++ theybothdieattheendActivity.java
@@ -0,0 +1,37 @@
+package com.example.books;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class theybothdieattheendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theybothdieatthend);
+        goBrowseBooks();
+        goRequestBooks();
+
+    }
+
+    private void goRequestBooks(){
+        Button button = (Button) findViewById(R.id.RequestBook);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(theybothdieattheendActivity.this, RequestActivity.class));
+            }
+        });
+    }
+
+    private void goBrowseBooks(){
+        Button button = (Button) findViewById(R.id.backButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(theybothdieattheendActivity.this, BrowseBooksActivity.class));
+            }
+        });
+    }
+}
