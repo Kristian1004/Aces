8832a041cdbf2017f9bce196472cd1f973da9673
Kristian1004 <98990666+Kristian1004@users.noreply.github.com>
2022-04-02 19:34:28 -0400

    Update MenuscreenActivity.java

--- MenuscreenActivity.java
+++ MenuscreenActivity.java
@@ -2,7 +2,11 @@ package com.example.minilibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MenuscreenActivity extends AppCompatActivity {
 
@@ -10,5 +14,51 @@ public class MenuscreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menuscreen);
+
+        goContactScreen();
+        goBackBooks();
+        goRequestBooks();
     }
+
+    private void goContactScreen(){
+        Button button = (Button) findViewById(R.id.ContactButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuscreenActivity.this, ContactActivity.class));
+            }
+        });
+    }
+
+    private void goBackBooks(){
+        Button button = (Button) findViewById(R.id.BackButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuscreenActivity.this, BrowseBooksActivity.class));
+            }
+        });
+    }
+
+
+    private void goRequestBooks(){
+        Button button = (Button) findViewById(R.id.RequestButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuscreenActivity.this, RequestActivity.class));
+            }
+        });
+    }
+
+    private void goMainMenu(){
+        Button button = (Button) findViewById(R.id.RequestButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuscreenActivity.this, MainActivity.class));
+            }
+        });
+    }
+    
 }
